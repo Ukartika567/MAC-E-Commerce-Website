bdc00bd29f2b34a383f0be3f6646581f104ed619
Usha <ushacec1234@gmail.com>
2021-07-17 03:47:40 -0700

    third commit

--- shop/views.py
+++ shop/views.py
@@ -129,7 +129,7 @@ def checkout(request):
                 'INDUSTRY_TYPE_ID':'Retail',
                 'WEBSITE':'WEBSTAGING',
                 'CHANNEL_ID':'WEB',
-                'CALLBACK_URL':'https://myawesomecart56.herokuapp.com/shop/handlerequest/'
+                'CALLBACK_URL':'http:127.0.0.1:8000/shop/handlerequest/'
 
             }
             param_dict['CHECKSUMHASH']= Checksum.generate_checksum(param_dict, MERCHANT_KEY)
